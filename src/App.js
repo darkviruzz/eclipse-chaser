866c96e492782548b0997652926c663aee78195a
darkviruzz <55845274+darkviruzz@users.noreply.github.com>
2025-07-28 08:36:25 -0700

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App(){
+  return (
+    <div styl={{ padding: '1rem' }}>
+      <h1>Eclipse Chaser</h1>
+      <p>The project is under development. See <code>ROADMAP.md</code> for next steps.</p>
+    </div>
+  );
+}
+
+export default App;
